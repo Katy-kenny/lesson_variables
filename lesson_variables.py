a91861d1cb5b849cd85c15f2f7862ca833e35a8e
Katy-kenny <e.i.volskaia@gmail.com>
2024-08-25 18:56:11 +0300

    Add files via upload

---
+++ lesson_variables.py
@@ -0,0 +1,5 @@
+number_of_completed_homeworks = 12
+number_of_hours = 1.5
+course_name = 'Phyton'
+time_for_one_tasks = (number_of_hours / number_of_completed_homeworks)
+print('Курс:', course_name, ', всего задач:', number_of_completed_homeworks, ', затрачено часов:', number_of_hours,', среднее время выполнения', time_for_one_tasks,'часа.')
